2522e3d8653021f543b9be1451588759b6a1cf8f
MiMii276 <sotillomaerona@gmail.com>
2025-11-11 20:11:33 +0800

    Start Activity 2: initial setup

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,15 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class DashboardController extends AbstractController
+class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $roles = $user ? $user->getRoles() : [];
+        if (in_array('ROLE_ADMIN', $roles)) {
+            $message = 'You are logged in as Admin. You have full access.';
+        } elseif (in_array('ROLE_USER', $roles)) {
+            $message = 'You are logged in as Regular User. Limited access granted.';
+        } else {
+            $message = 'Welcome!';
+        }
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'message' => $message,
+            'username' => $user ? $user->getUserIdentifier() : 'Guest',
         ]);
     }
 }
